f99ee3ed1b633264bb860385e6f9065b2c1044fe
Steve Kavuu <mrkavuu@gmail.com>
2020-12-15 10:50:59 -0600

    2

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+
+
+function Navbar() {
+    return (
+        <>
+          <nav className="navbar">
+              </nav>  
+        </>
+    )
+}
+
+export default Navbar
